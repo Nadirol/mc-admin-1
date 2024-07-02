838dfdcab789ad040b15e3a6849b7e09d078eb0e
Khanh Duy <kristalz139@gmail.com>
2024-07-02 11:56:29 +0700

    update

--- admin/schemas/index.ts
+++ admin/schemas/index.ts
@@ -1,3 +1,4 @@
+import bannerXHH from "./bannerXHH";
 import order from "./order";
 import orderXHH from "./orderXHH";
 import policy from "./policy";
@@ -6,4 +7,4 @@ import postXHH from "./postXHH";
 import product from "./product";
 import productPostXHH from "./productPostXHH";
 
-export const schemaTypes = [post, product, order, policy, postXHH, productPostXHH, orderXHH]
+export const schemaTypes = [post, product, order, policy, postXHH, productPostXHH, orderXHH, bannerXHH]
